d0e04a6e366508fabb2f4fce49c4b8e43bce47c6
SaiBotta <bottasai@gmail.com>
2019-05-18 14:17:16 +0530

    commit

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ app.get('/:id/:a', (req,res)=>{
     res.send(req.params);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3200;
 app.listen(port,() => {console.log(`Listening on port${port}`)});
+
